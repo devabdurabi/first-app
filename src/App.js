f3fcac7d2fe149f126b5a237cc9b00be7fabeac9
Abdurabi <amabukar070@gmail.com>
2023-02-08 20:34:58 +0100

    hero 2 verwijdert en alles opgeslagenwerkwijze aangemaakt

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Werkwijze from "./components/Werkwijze";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Werkwijze />
     </div>
   );
 }
--- src/components/Werkwijze.jsx
+++ src/components/Werkwijze.jsx
@@ -1,8 +1,19 @@
  import React from 'react'
+ import Laptop from '../assets/laptop.jpeg'
 
 const Werkwijze = () => {
   return (
-    <div>Werkwijze</div>
+    <section className='w-full bg-white py-16 px-4'>
+        <div className=' max-w-[1240px] mx-auto my-4 grid md:grid-cols-2'>
+            <img className='w-[500px] mx-auto my-4' src={Laptop} alt="" />
+            <div className='flex flex-col justify-center'>
+                <p className='text-[#00df9a] font-bold'>Beste wedstrijd schema</p>
+                <h1 className='md-text-4xl sm:text-3xl text-2xl font-bold py-2'>Ontdek alle voordelen 2.0</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut eum incidunt quasi atque hic vel animi sit amet consectetur adipisicing elit. Ut eum incidunt quasi atque hic vel an qui?</p>
+                <button className='bg-[#aadf4a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Meld aan</button>
+            </div>
+        </div>
+    </section>
   )
 }
 
