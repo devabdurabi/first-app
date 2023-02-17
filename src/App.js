07f63430f3b93ff7eee53dcf4826e0d3db85ddee
Abdurabi <amabukar070@gmail.com>
2023-02-17 14:55:39 +0100

    de kaarten gedeelte afgerond en foto's toegevoedgd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
+import Cards from "./components/Cards";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Newsletter from "./components/Newsletter";
 import Werkwijze from "./components/Werkwijze";
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
       <Navbar />
       <Hero />
       <Werkwijze />
+      <Newsletter />
+      <Cards />
     </div>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import bgImg from '../assets/cloud.png'
+import Werk from '../assets/Werk.jpg'
 import Typed from 'react-typed'
 
 const Hero = () => {
   return (
-    <section className='w-full h-screen py-20 bg-zinc-200 flex flex-col justify-between'>
+    <section className='flex flex-col justify-between w-full h-screen py-20 bg-white'>
         <div className='grid md:grid-cols-2 max-w-[1240px] mx-auto'>
-            <div className='flex flex-col justify-center md:items-start w-full px-2 py-8'>
+            <div className='flex flex-col justify-center w-full px-2 py-8 md:items-start'>
                 <p className='text-2xl'>Welkom bij de AcroApp 2.0</p>
-                <h1 className='py-3 text-5xl md:text-7xl font-bold'>Acrobatic<hr></hr> Solutions for <Typed strings={['Studenten', 'Atleten', 'Beginners']} typeSpeed={120} backSpeed={140} loop  /> </h1>
+                <h1 className='py-3 text-5xl font-bold md:text-7xl'>Acrobatic <br></br>Solutions or <Typed strings={['Studenten', 'Atleten', 'Beginners']} typeSpeed={120} backSpeed={140} loop  /> </h1>
                 
                 <p className='text-1xl '>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident et, ad eos adipisci esse molestias, corrupti id mollitia quam animi ab rerum?</p>
             
@@ -18,7 +18,7 @@ const Hero = () => {
 
 
             <div>
-                <img src={bgImg} alt="" />
+                <img src={Werk} alt="" />
             </div>
         </div>
     </section>
